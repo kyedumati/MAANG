081e113f8363a85e06805591b88a0c2beee8eef2
kasiy <kasi.yedumati@gmail.com>
2025-05-04 17:17:48 +0530

    pattern programs

---
+++ patterns/src/CenterTrianglePattern.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CenterTrianglePattern {
+
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0; i<n; i++) {
+            //spaces
+            for(int j=0; j<n-i-1; j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int j=0; j<2*i+1; j++) {
+                System.out.print("*");
+            }
+            //spaces
+            for(int j=0; j<n-i-1; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
